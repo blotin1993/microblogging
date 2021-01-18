e4360bba94734c19ef2145a7eba8236cb3494d6c
Gonzalo <blottagonzalo@gmail.com>
2021-01-18 13:31:37 -0300

    solved bug

--- bd/conexionBD.go
+++ bd/conexionBD.go
@@ -10,9 +10,10 @@ import (
 
 /*MongoCN es el objeto de conexión a la base de datos*/
 var MongoCN = ConectarBD()
-var clientOptions = options.Client().ApplyURI("mongodb+srv://admin:opeZRi2jdKIBXcmb@microblogging.p49ri.mongodb.net/test?authSource=admin&replicaSet=atlas-5u9fsz-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true")
+var clientOptions = options.Client().ApplyURI("mongodb+srv://usuario_1:SkillFactory2021@cluster0.5k8zj.mongodb.net/microblog?retryWrites=true&w=majority")
 
-/*ConectarBD es la función que me permite conectar a la base de datos*/
+/*ConectarBD es la función que me permite conectar a la base de datos
+  Devuelve una conexión a la BD del tipo Mongo Client*/
 func ConectarBD() *mongo.Client {
 	client, err := mongo.Connect(context.TODO(), clientOptions)
 	if err != nil {
